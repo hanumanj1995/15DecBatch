fec280c6ace4ed75189964a5ac713dec050a1bf5
hanumanj1995 <101492201+hanumanj1995@users.noreply.github.com>
2022-12-16 01:07:46 +0530

    Update Demo1.java

--- 15DecEclipseJavaProject/src/com/qa/Demo1.java
+++ 15DecEclipseJavaProject/src/com/qa/Demo1.java
@@ -4,6 +4,15 @@ public class Demo1 {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Arun");
+		int i=1;
+	       public void subofmain()
+	       {
+		       if(a<=5)
+		       {
+		           system.out.println()
+		       }
+	       }
+		
 
 	}
 
