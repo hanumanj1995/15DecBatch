b720cc0c0483f2a3057a6e95274ad1f5e52457d5
Hanuman <hanumanj1995@gmail.com>
2022-12-16 01:14:25 +0530

    Update Demo1.java

--- 15DecEclipseJavaProject/src/com/qa/Demo1.java
+++ 15DecEclipseJavaProject/src/com/qa/Demo1.java
@@ -1,18 +1,22 @@
 package com.qa;
 
 public class Demo1 {
-
+	
+	int i=1;
+    public void subofmain()
+    {
+	       if(i<=5)
+	       {
+	          System.out.println(i);
+	          i++;
+	          subofmain();
+	       }
+    }
 	public static void main(String[] args) {
 		System.out.println("Hello Arun");
-		int i=1;
-	       public void subofmain()
-	       {
-		       if(a<=5)
-		       {
-		           system.out.println()
-		       }
-	       }
 		
+		Demo1 d=new Demo1();
+		d.subofmain();
 
 	}
 
